c2353b115fe2f582a7c692edb174e58efbf313b7
laripeanuts <larissarabelolf@gmail.com>
2023-01-31 22:10:21 -0300

    feat: adds pages

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/laripeanuts.png",
+      name: "Larissa Rabelo",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content: "Oi pessoal, acabei de subir mais um projeto 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/sabrinagomesb.png",
+      name: "Sabrina Gomes",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Oi, pessoal 👋" },
+      {
+        type: "paragraph",
+        content: "Muito massa o que o javascript consegue fazer 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-10 20:00:00"),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
